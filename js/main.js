d403ad14fda1de53a2a573f0f401e6a7a828fe5f
Bartek Wieckowski <bart.it.pro@gmail.com>
2021-08-05 20:05:27 +0200

    refactoring code

--- js/main.js
+++ js/main.js
@@ -1,21 +1,21 @@
+setInterval(clockSetup, 1000);
 const hour = document.querySelector("#hand-hour");
 const minutes = document.querySelector("#hand-minutes");
 const seconds = document.querySelector("#hand-seconds");
 
-const clockSetup = function () {
-  const date = new Date();
-  const secondsRatio = date.getSeconds() / 60;
-  const minutesRatio = (secondsRatio + date.getMinutes()) / 60;
-  const hourRatio = minutesRatio + date.getHours() / 12;
+function clockSetup() {
+  const currDate = new Date();
+  const secondsRatio = currDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currDate.getMinutes()) / 60;
+  const hourRatio = minutesRatio + currDate.getHours() / 12;
 
   setRotate(seconds, secondsRatio);
   setRotate(minutes, minutesRatio);
   setRotate(hour, hourRatio);
-};
+}
 
-const setRotate = function (item, rotateRatio) {
-  item.style.setProperty("--rotate", rotateRatio * 360);
-};
+function setRotate(element, rotateRatio) {
+  element.style.setProperty("--rotate", rotateRatio * 360);
+}
 
-setInterval(clockSetup, 1000);
 clockSetup();
